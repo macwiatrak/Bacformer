dc4cd26d960f90f4886963f27e1d45f1e605623a
macwiatrak <macwiatrak@gmail.com>
2025-05-27 18:19:00 +0100

    fix: fix gene_essentiality_prediction.py

--- bacformer/modeling/modeling_tasks.py
+++ bacformer/modeling/modeling_tasks.py
@@ -64,8 +64,9 @@ class BacformerForProteinClassification(BacformerPreTrainedModel):
             print("Special tokens mask shape:", special_tokens_mask.shape)
             print("Logits shape:", logits.shape)
             logits = logits[special_tokens_mask == self.config.prot_emb_token_id]
+            labels = labels[special_tokens_mask == self.config.prot_emb_token_id]
             print("Filtered logits shape:", logits.shape)
-            print(special_tokens_mask.unique())
+            print("Labels shape:", labels.shape)
 
             if self.config.problem_type == "regression":
                 loss = mse_loss(logits, labels)
